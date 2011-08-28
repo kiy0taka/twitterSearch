11fa36193f216ebeb1e01d92dea0b2d4bb199e7f
kiy0taka <kiy0taka333+github@gmail.com>
2011-08-28 16:20:26 +0900

    Tiwtter検索実装

--- griffon-app/controllers/twittersearch/TwitterSearchController.groovy
+++ griffon-app/controllers/twittersearch/TwitterSearchController.groovy
@@ -1,15 +1,15 @@
 package twittersearch
 
+import twitter4j.*
+
 class TwitterSearchController {
 
     def model
     def view
+    def twitter
 
     def search = {
-        model.tweets = [
-            [fromUser:'kiy0taka', text:'Griffonなう。'],
-            [fromUser:'kiy0taka', text:'@groovybook Gマガ読んだよ！']
-        ]
+        model.tweets = twitter.search(new Query(model.searchText)).tweets
         view.searchResult.fireTableDataChanged()
     }
 }
